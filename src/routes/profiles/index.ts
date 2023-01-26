b7996fdd536414579cf7cada0456717a9a9a5767
Tatsiana Sapun <t.sapun@pixelplex.io>
2023-01-26 23:40:12 +0100

    feat: implement profile logic

--- src/routes/profiles/index.ts
+++ src/routes/profiles/index.ts
@@ -2,13 +2,16 @@ import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-sc
 import { idParamSchema } from '../../utils/reusedSchemas';
 import { createProfileBodySchema, changeProfileBodySchema } from './schema';
 import type { ProfileEntity } from '../../utils/DB/entities/DBProfiles';
+import { validate } from 'uuid';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
   fastify.get('/', async function (request, reply): Promise<
     ProfileEntity[]
-  > {});
+  > {
+    return this.db.profiles.findMany();
+  });
 
   fastify.get(
     '/:id',
@@ -17,7 +20,17 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      const { id } = request.params;
+      if (!validate(id)) {
+        throw this.httpErrors.notFound();
+      }
+      const profile = await this.db.profiles.findOne({ key: 'id', equals: id });
+      if (!profile) {
+        throw this.httpErrors.notFound();
+      }
+      return profile;
+    }
   );
 
   fastify.post(
@@ -27,7 +40,17 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: createProfileBodySchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      const { body } = request;
+      if (!['basic', 'business'].includes(body.memberTypeId)) {
+        throw this.httpErrors.badRequest();
+      }
+      const conflict = await this.db.profiles.findOne({ key: 'userId', equals: body.userId});
+      if (conflict) {
+        throw this.httpErrors.badRequest();
+      }
+      return this.db.profiles.create(body);
+    }
   );
 
   fastify.delete(
@@ -37,7 +60,17 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      const { id } = request.params;
+      if (!validate(id)) {
+        throw this.httpErrors.badRequest();
+      }
+      const profile = await this.db.profiles.findOne({ key: 'id', equals: id });
+      if (!profile) {
+        throw this.httpErrors.notFound();
+      }
+      return this.db.profiles.delete(id);
+    }
   );
 
   fastify.patch(
@@ -48,7 +81,17 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      const { body, params: { id } } = request;
+      if (!validate(id)) {
+        throw this.httpErrors.badRequest();
+      }
+      const profile = await this.db.profiles.findOne({ key: 'id', equals: id });
+      if (!profile) {
+        throw this.httpErrors.notFound();
+      }
+      return this.db.profiles.change(id, body);
+    }
   );
 };
 
