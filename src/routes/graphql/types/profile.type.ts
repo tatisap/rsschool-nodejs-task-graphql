d5f8533d50397231e0aa6d41906fe43afad8fb25
Tatsiana Sapun <t.sapun@pixelplex.io>
2023-01-28 23:59:19 +0100

    feat: implement graphql profile type

---
+++ src/routes/graphql/types/profile.type.ts
@@ -0,0 +1,22 @@
+import { GraphQLNonNull, GraphQLObjectType, GraphQLString, GraphQLID } from 'graphql';
+import DB from '../../../utils/DB/DB';
+import { memberType, memberTypeIdEnum } from './member.type';
+
+export const profileType: GraphQLObjectType = new GraphQLObjectType<any, DB>({
+  name: 'Profile',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLID) },
+    avatar: { type: new GraphQLNonNull(GraphQLString) },
+    sex: { type: new GraphQLNonNull(GraphQLString) },
+    birthday: { type: new GraphQLNonNull(GraphQLString) },
+    country: { type: new GraphQLNonNull(GraphQLString) },
+    street: { type: new GraphQLNonNull(GraphQLString) },
+    city: { type: new GraphQLNonNull(GraphQLString) },
+    memberTypeId: { type: new GraphQLNonNull(memberTypeIdEnum) },
+    userId: { type: new GraphQLNonNull(GraphQLString) },
+    memberType: {
+      type: memberType,
+      resolve: (profile, _a, context) => context.memberTypes.findOne({ key: 'id', equals: profile.memberTypeId }),
+    }
+  }),
+});
